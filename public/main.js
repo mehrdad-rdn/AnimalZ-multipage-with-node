080cac929e676075b7cf8f1cfe424d7a607151c2
mehrdad rdn <radnia.mehrdad@gmail.com>
2023-09-10 23:04:13 +0430

    initial commit

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn')
+const title = document.getElementById('title')
+const logText = document.getElementById('logText')
+btn.addEventListener('click', (e)=>{
+    
+})
