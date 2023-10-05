09379d9b54bed2ab11f873eba1e61114b26528fc
mehrdad rdn <radnia.mehrdad@gmail.com>
2023-10-05 22:41:29 +0330

    add back-to-top button and see less link in accordions

--- public/main.js
+++ public/main.js
@@ -1,6 +1,36 @@
-const btn = document.getElementById('btn')
-const title = document.getElementById('title')
-const logText = document.getElementById('logText')
-btn.addEventListener('click', (e)=>{
-    
+const backTopBtn = document.getElementById('back-top-btn')
+
+addEventListener("scroll", (e)=>{
+    backTopBtn.style.display = (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) ? 'block' : 'none' ;
 })
+
+//<-- Second method -->
+
+// backTopBtn.addEventListener("click", ()=>{
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// })
+let firstScroll = {}
+const seeMore = document.getElementsByClassName('seeMore-link')
+const seeLess = document.getElementsByClassName('seeLess-link') 
+const seeMoreArr = Array.from(seeMore)
+const seeLessArr = Array.from(seeLess)
+console.log(seeMore);
+console.log(seeLessArr);
+seeMoreArr.forEach(element => {
+    element.addEventListener('click', e => {
+        const targetVal = element.dataset.bsTarget
+        console.log('seeMore target:' , targetVal);
+        firstScroll[targetVal] = (document.documentElement.scrollTop || document.body.scrollTop)
+        console.log('first scroll value:', firstScroll);
+    });
+});
+seeLessArr.forEach(element=> {
+    element.addEventListener('click', e=> {
+    const targetVal = element.dataset.bsTarget
+    console.log('seeMLess target:' , targetVal);
+    document.documentElement.scrollTop = firstScroll[targetVal]
+    // document.body.scrollTop = firstScroll.targetVal-100
+    })
+})
+
