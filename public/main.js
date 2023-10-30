b69bcd92647201e2ae0e8fd6cd14bdefce38a8b7
mehrdad rdn <radnia.mehrdad@gmail.com>
2023-10-30 22:45:53 +0330

    add dog page and some changes in blog and dogBreed pages

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
 const backTopBtn = document.getElementById('back-top-btn')
-
+let lastScrollTop = 0;
+const navbar = document.querySelector('nav.navbar')
+console.log('navbar:', navbar);
 addEventListener("scroll", (e)=>{
-    backTopBtn.style.display = (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) ? 'block' : 'none' ;
+    currentScrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop
+    if (currentScrollTop < lastScrollTop) {
+        console.log('scrolling up');
+        backTopBtn.style.display = ((currentScrollTop > 300) ) ? 'block' : 'none'
+    } else if (currentScrollTop > lastScrollTop) {
+        console.log('scrolling down');
+        backTopBtn.style.display = 'none'
+    }
+    lastScrollTop = (currentScrollTop <= 0) ? 0 : currentScrollTop
 })
 
 //<-- Second method -->
@@ -15,22 +25,48 @@ const seeMore = document.getElementsByClassName('seeMore-link')
 const seeLess = document.getElementsByClassName('seeLess-link') 
 const seeMoreArr = Array.from(seeMore)
 const seeLessArr = Array.from(seeLess)
-console.log(seeMore);
-console.log(seeLessArr);
 seeMoreArr.forEach(element => {
     element.addEventListener('click', e => {
         const targetVal = element.dataset.bsTarget
-        console.log('seeMore target:' , targetVal);
         firstScroll[targetVal] = (document.documentElement.scrollTop || document.body.scrollTop)
-        console.log('first scroll value:', firstScroll);
     });
 });
 seeLessArr.forEach(element=> {
     element.addEventListener('click', e=> {
     const targetVal = element.dataset.bsTarget
-    console.log('seeMLess target:' , targetVal);
     document.documentElement.scrollTop = firstScroll[targetVal]
-    // document.body.scrollTop = firstScroll.targetVal-100
     })
 })
 
+// breed characteristics collapse 
+const charCollapseBtns = Array.from(document.getElementsByClassName("char-collapse"))
+const charCollapseParagraphs = Array.from(document.getElementsByClassName("char-collapse-target"))
+
+charCollapseBtns.forEach(element=>{
+    const identifier = element.dataset.charDescid;
+    element.addEventListener('click', (e)=>{
+        const description = document.getElementById(identifier);
+        description.style.display = (description.style.display=='none')? 'block' : 'none';
+            element.classList.toggle('bi-plus')
+            element.classList.toggle('bi-dash')
+    })
+})
+charCollapseParagraphs.forEach(element=>{
+
+})
+
+// toggle nav-tab visibility
+const tabAnchors = document.querySelectorAll('.nav-tabs > .nav-item > a')
+let lastActiveTab = 'undefined'
+tabAnchors.forEach((item) => {
+   if(lastActiveTab == 'undefined'){
+   (Array.from(item.classList).includes('active')) && (lastActiveTab = item)} 
+   item.addEventListener('click', (e)=>{
+    e.preventDefault()
+    document.getElementById(e.target.dataset.tabRef).style.display = 'block'
+    document.getElementById(lastActiveTab.dataset.tabRef).style.display = 'none'
+    lastActiveTab.classList.toggle('active')
+    e.target.classList.toggle('active')
+    lastActiveTab = e.target
+   }) 
+})
